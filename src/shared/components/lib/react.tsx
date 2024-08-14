8157d11774791a49979dd1a59b322c0180b6e1a0
Nikolay Smyk <157750278+SmykNikolay@users.noreply.github.com>
2024-08-14 14:12:48 +0300

    chore: Add initial project configuration and dependencies

---
+++ src/shared/components/lib/react.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect, startTransition } from "react";
+
+export function useAppearanceDelay(
+  show?: boolean,
+  options = {} as {
+    defaultValue?: boolean;
+    appearanceDelay?: number;
+    minDisplay?: number;
+  },
+) {
+  const {
+    minDisplay = 500,
+    defaultValue = false,
+    appearanceDelay: appearanceDelay = 500,
+  } = options;
+
+  const [delayedShow, setDelayedShow] = useState(defaultValue);
+
+  useEffect(() => {
+    if (show) {
+      const timer = setTimeout(() => {
+        startTransition(() => setDelayedShow(true));
+      }, appearanceDelay);
+      return () => clearTimeout(timer);
+    } else {
+      const timer = setTimeout(() => {
+        startTransition(() => setDelayedShow(false));
+      }, minDisplay);
+      return () => clearTimeout(timer);
+    }
+  }, [appearanceDelay, show, minDisplay]);
+
+  return delayedShow;
+}
